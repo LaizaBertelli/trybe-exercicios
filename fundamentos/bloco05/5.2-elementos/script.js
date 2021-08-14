f4c9ca69dd3913e3e7dd2b7b854bc3181caf689c
LaizaBertelli <laizabertelli@hotmail.com>
2021-08-14 18:08:47 -0300

    Creates tag main

--- fundamentos/bloco05/5.2-elementos/script.js
+++ fundamentos/bloco05/5.2-elementos/script.js
@@ -4,3 +4,8 @@ var body = document.body;
 let title = document.createElement('h1');
 title.innerText = 'Exercício 5.2 - JavaScript DOM'
 body.appendChild(title);
+
+//cria uma tag main
+let main = document.createElement('main');
+main.classList.add("main-content");
+body.appendChild(main);
