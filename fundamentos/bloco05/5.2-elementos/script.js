913f353afe8e595e870314d30e3d2bfd1c0e4e2a
LaizaBertelli <laizabertelli@hotmail.com>
2021-08-14 18:13:20 -0300

    Creates central section

--- fundamentos/bloco05/5.2-elementos/script.js
+++ fundamentos/bloco05/5.2-elementos/script.js
@@ -1,11 +1,20 @@
 var body = document.body;
+var text = [
+  'Exercício 5.2 - JavaScript DOM',
+  'Algum texto'
+]
 
 //cria uma tag h1 contendo um título
 let title = document.createElement('h1');
-title.innerText = 'Exercício 5.2 - JavaScript DOM'
+title.innerText = text[0];
 body.appendChild(title);
 
 //cria uma tag main
 let main = document.createElement('main');
 main.classList.add("main-content");
 body.appendChild(main);
+
+// adiciona uma section central
+let centerSection = document.createElement('section');
+section.classList.add("center-content");
+main.appendChild(centerSection);
