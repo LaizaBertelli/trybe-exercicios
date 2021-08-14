a9f41de9b8c9fa04f7b4b7330d29f7082ce27ecc
LaizaBertelli <laizabertelli@hotmail.com>
2021-08-14 18:07:39 -0300

    Creates tag h1

---
+++ fundamentos/bloco05/5.2-elementos/script.js
@@ -0,0 +1,6 @@
+var body = document.body;
+
+//cria uma tag h1 contendo um título
+let title = document.createElement('h1');
+title.innerText = 'Exercício 5.2 - JavaScript DOM'
+body.appendChild(title);
