57f2176ba0f746bfbcbd61967526f766ba71ed7d
LaizaBertelli <laizabertelli@hotmail.com>
2021-08-15 14:38:03 -0300

    Resolution of bonus exercise 4, day 4.3

--- fundamentos/bloco04/4.3-js-logica-e-algoritmos/script.js
+++ fundamentos/bloco04/4.3-js-logica-e-algoritmos/script.js
@@ -7,7 +7,7 @@ function square(n){
     }
     //imprime aux n vezes
     for(var i = 0; i <n; i++){
-        console.log(aux);
+        // console.log(aux);
     }
 }
 
@@ -19,7 +19,7 @@ function triangle(n){
     //adiciona i "*" e imprime aux
     for(var i =0; i < n; i++){
         aux += "*";
-        console.log(aux);
+        // console.log(aux);
     }
 }
 triangle(5);
@@ -35,9 +35,55 @@ function invertedTriangle(n){
     // a cada passagem ele move um indice para a esquerda
     for(var j = n; j > 0; j--){
         aux[j-1] = "*";
-        console.log(aux.join("")); 
+        // console.log(aux.join("")); 
     }
 }
 invertedTriangle(5);
 
+function preencheLinha(num){
+    let string = '';
+    for(let i = 0; i < num; i ++){
+        string += '*';
+    }
+    return string;
+}
+
+function revert(array){
+    for( let i = array.length - 1; i >= 0; i --){
+        console.log(array[i]);
+    }
+}
+function addSpaces (line){
+    let array = [];
+    for(let i = 0; i < line; i ++){
+        array.push(' ');
+    }
+    return array.join('');
+}
 //exercicio 4
+function baseN(n){
+
+    let lines = (n + 1) / 2;
+    let controle = lines;
+    let piramide = [];
+    let base = n;
+    
+    //preencher linhas
+    for(let i = 0; i < lines; i ++){
+        if(lines === controle){
+            piramide[i] = preencheLinha(base);
+            base = base - 2;
+            controle -= 1;
+        }else {
+            piramide[i] = addSpaces(lines - controle) + preencheLinha(base);
+            base = base - 2;
+            controle -= 1;
+        }
+    }
+    revert(piramide);
+}
+
+baseN(7);
+
+
+
