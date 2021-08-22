adfeda47b4e282e6bf97935da32168d60f5bbd78
LaizaBertelli <laizabertelli@hotmail.com>
2021-08-22 12:24:08 -0300

    Adds input values in a new div

---
+++ fundamentos/bloco06/6.1-forms/script.js
@@ -0,0 +1,77 @@
+// Dados pessoais do usuário
+const userNameInput = document.getElementById('user-name');
+const userEmailInput = document.getElementById('user-email');
+const userCPFInput = document.getElementById('user-cpf');
+const userAddressInput = document.getElementById('user-address');
+const userCityInput = document.getElementById('user-city');
+//general elements
+const body = document.body;
+const answers = document.getElementById('answers');
+const ulUserData = document.getElementById('user-data');
+const ulJobData = document.getElementById('last-job');
+
+//dados do último emprego;
+const curriculumInput = document.getElementById('curriculum');
+const jobInput = document.getElementById('job');
+const jobDescriptionInput = document.getElementById('job-description');
+const startDateInput = document.getElementById('start-date');
+
+function prevent(event) {
+  const preventEvent = event;
+  preventEvent.preventDefault();
+}
+
+function createUserInfos(userInfos) {
+  for(let key in userInfos){
+    userInfos.key;
+    let li = document.createElement('li');
+    li.innerText = userInfos[key];
+    ulUserData.appendChild(li);
+    console.log(userInfos[key]);
+  }
+ }
+
+function createLastJobInfos (lastJob){
+  for(let key in lastJob) {
+    let li = document.createElement('li');
+    li.innerText = lastJob[key];
+    ulJobData.appendChild(li);
+  }
+}
+
+function getUserValues() {
+  let userData = {
+    name: userNameInput.value,
+    email: userEmailInput.value,
+    cpf: userCPFInput.value,
+    addres: userAddressInput.value,
+    city: userCityInput.value,
+  };
+  console.log(userData);
+  createUserInfos(userData);
+}
+
+function getLastJobValues() {
+  let jobData = {
+    curriculum: curriculumInput.value,
+    job: jobInput.value,
+    jobDescription: jobDescriptionInput.value,
+    startDate: startDateInput.value,
+  };
+  console.log(jobData);
+  createLastJobInfos(jobData);
+}
+
+function addEvent() {
+  const preventButton = document.querySelector('.submit-button');
+  preventButton.addEventListener('click', prevent);
+  preventButton.addEventListener('click', getUserValues);
+  preventButton.addEventListener('click', getLastJobValues);
+}
+
+addEvent();
+
+/* O que está faltando?
+ Fazer verificações em cada valor
+ criar um alert se os dados não forem válidos
+ */
