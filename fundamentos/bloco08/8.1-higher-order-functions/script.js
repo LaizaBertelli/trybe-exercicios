2c59b2a395a42690ac468a7ef4a618f814c16fdc
LaizaBertelli <laizabertelli@hotmail.com>
2021-09-02 15:16:46 -0300

    Fix exercise 1

--- fundamentos/bloco08/8.1-higher-order-functions/script.js
+++ fundamentos/bloco08/8.1-higher-order-functions/script.js
@@ -1,29 +1,40 @@
-function generateEmail (name, lastName) {
-  return `${name}_${lastName}@trybe.com`;
+function generateEmail (name) {
+  const completeName = name.split(' ');
+  const email = `${completeName.join('_')}@trybe.com`;
+  const userInfos = {
+    completeName: completeName,
+    email: email,
+  };
+  return userInfos;
 }
 
-const newEmployees = () => {
+const newEmployees = (callback) => {
   const employees = {
-    id1: generateEmail('Pedro', 'Guerra'), // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
-    id2: generateEmail('Luiza', 'Drumon'), // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
-    id3: generateEmail('Carla', 'Paiva'), // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
+    id1: callback('Laiza Bertelli'), // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
+    id2: callback('Luiza Drumon'), // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
+    id3: callback('Carla Paiva'), // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
   }
   return employees;
 };
 
-// console.log(newEmployees());
+console.log(newEmployees(generateEmail));
 
-function random() {
-  const min = 1;
-  const max = 5;
-  let rand =  Math.random() * (max - min) + min;
-  return Math.round(rand);
-}
-function check(guess, func) {
-  if(guess === func) {
-    return 'Parabéns você ganhou';
-  }
-  return 'Tente novamente';
-}
+// function random() {
+//   const min = 1;
+//   const max = 5;
+//   let rand =  Math.random() * (max - min) + min;
+//   return Math.round(rand);
+// }
+// function check(guess, func) {
+//   if(guess === func) {
+//     return 'Parabéns você ganhou';
+//   }
+//   return 'Tente novamente';
+// }
 
-console.log(check(3, random()));
+// // console.log(check(3, random()));
+
+// const RIGHT_ANSWERS = ['A', 'C', 'B', 'D', 'A', 'A', 'D', 'A', 'D', 'C'];
+// const STUDENT_ANSWERS = ['A', 'N.A', 'B', 'D', 'A', 'C', 'N.A', 'A', 'D', 'B'];
+
+
