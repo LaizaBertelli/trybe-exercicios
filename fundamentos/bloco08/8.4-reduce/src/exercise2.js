d92e509b352df4d78870a71bd62907575e83750e
LaizaBertelli <laizabertelli@hotmail.com>
2021-09-09 16:27:23 -0300

    Adds initial files

---
+++ fundamentos/bloco08/8.4-reduce/src/exercise2.js
@@ -0,0 +1,7 @@
+const expectedResult = "George R. R. Martin, J. R. R. Tolkien, Isaac Asimov, Frank Herbert, Stephen King, H. P. Lovecraft.";
+
+function reduceNames() {
+  // escreva seu código aqui
+}
+
+assert.strictEqual(reduceNames(), expectedResult);
---
+++ fundamentos/bloco08/8.4-reduce/src/exercise3.js
@@ -0,0 +1,7 @@
+const expectedResult = 43;
+
+function averageAge() {
+  // escreva seu código aqui
+}
+
+assert.strictEqual(averageAge(), expectedResult);
