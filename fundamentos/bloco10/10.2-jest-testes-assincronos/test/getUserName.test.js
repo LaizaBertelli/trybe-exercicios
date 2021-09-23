f78cd1b46e9e969d25f21891fbd048ba997b31e5
LaizaBertelli <laizabertelli@hotmail.com>
2021-09-23 18:04:10 -0300

    Implements tests for function getUserName with async/await syntax

--- fundamentos/bloco10/10.2-jest-testes-assincronos/test/getUserName.test.js
+++ fundamentos/bloco10/10.2-jest-testes-assincronos/test/getUserName.test.js
@@ -7,9 +7,25 @@ describe('Tests function getUserName with promise syntax', () => {
     });
   });
 
-  it('test when getUserName fails finding a user', () => {
+  it('test when getUserName fails finding an user', () => {
     getUserName(3).catch((error) => (
       expect(error.message).toMatch(`User with 3 not found.`)
     ));
+  });
+});
+
+describe('Tests function getUserName with async/await syntax', () => {
+  it('succeed', async () => {
+    const user = await getUserName(2);
+  expect(user).toBe('Paul') ;
+  });
+
+  it('fails', async () => {
+    expect.assertions(1);
+    try {
+      await getUserName(4);
+    } catch (error) {
+      expect(error).toEqual(new Error('User with 4 not found.'));
+    }
   });
 });
