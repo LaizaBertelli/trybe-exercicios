e9d8b1bb34c5cbdb9b9b9bb1a87fdabd851e3f83
LaizaBertelli <laizabertelli@hotmail.com>
2021-10-05 11:42:45 -0300

    Add component Header

---
+++ front-end/bloco11/11.1-introducao-a-react/front-end-contents/src/App.js
@@ -0,0 +1,13 @@
+
+import React from 'react';
+import './App.css';
+import Header from './Header';
+
+class App extends React.Component {
+  render() {
+    return (
+      <Header />
+    );
+  }
+}
+export default App;
